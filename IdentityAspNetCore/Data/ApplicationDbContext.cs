82516315ed2bac0aa98d226890fb3a9c898254ea
nemanjasubotic1 <103412864+nemanjasubotic1@users.noreply.github.com>
2024-11-09 17:22:57 +0100

    Add project files.

---
+++ IdentityAspNetCore/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using IdentityAspNetCore.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityAspNetCore.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+}
---
+++ IdentityAspNetCore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IdentityAspNetCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
